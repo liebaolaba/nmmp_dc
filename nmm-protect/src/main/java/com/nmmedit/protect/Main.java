afae3a7e509436f07652f1b9d1bc7248db7b0ac9
liebaolaba <102332468+liebaolaba@users.noreply.github.com>
2026-09-22 18:29:44 +0800

    Add files via upload

---
+++ nmm-protect/src/main/java/com/nmmedit/protect/Main.java
@@ -0,0 +1,42 @@
+package com.nmmedit.protect;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+//        args = new String[3];
+//        args[0] = "apk";
+//        args[1] = System.getProperty("user.dir") + "\\apptmp\\app_s.apk";
+//        args[2] = System.getProperty("user.dir") + "\\proclass.txt";
+        
+        if (args.length < 1) {
+            System.err.println("<apk|aab|aar> subCommand");
+            final String[] newArgs = new String[0];
+            System.err.println("apk:");
+            ApkMain.main(newArgs);
+            System.err.println("aab:");
+            AabMain.main(newArgs);
+            System.err.println("aar:");
+            AarMain.main(newArgs);
+            System.exit(-1);
+            return;
+        }
+        final String[] newArgs = Arrays.copyOfRange(args, 1, args.length);
+        switch (args[0]) {
+            case "apk":
+                ApkMain.main(newArgs);
+                break;
+            case "aab":
+                AabMain.main(newArgs);
+                break;
+            case "aar":
+                AarMain.main(newArgs);
+                break;
+            default:
+                System.err.println("Unknown subcommand");
+                System.exit(-1);
+        }
+    }
+}
